1aa7aea36d004b52f812b5f9fe3c3725bb11c2e6
Daniel Gading <dgading@gmail.com>
2020-08-11 10:47:54 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "rules": {
+        "react/jsx-curly-spacing": [2, "never", { "allowMultiline": true }],
+    },
+    "env": {
+        "jest": true,
+        "browser": true,
+    }
+};
