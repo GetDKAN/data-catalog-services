76cfc34d2941c992cf9a1f41e4511d957df49f5f
dgading <dgading@gmail.com>
2021-02-03 14:42:57 -0500

    Update tests to cover initial params and clear filters function

--- src/hooks/useSearchAPI/usesearchapi.test.jsx
+++ src/hooks/useSearchAPI/usesearchapi.test.jsx
@@ -104,4 +104,66 @@ describe('useSearchAPI Custom Hook', () => {
     );
     expect(result.current.pageSize).toEqual(25);
   });
+
+  test('tests resetFilters', async() => {
+    axios.get.mockImplementation(() => Promise.resolve(data_results));
+    const { result } = renderHook(() => useSearchAPI(rootUrl));
+    await act(async () => { jest.runAllTimers() });
+    expect(axios.get).toHaveBeenCalledWith(
+      `${rootUrl}/search/?`,
+    );
+    await act(async () => { result.current.updateSelectedFacets({key: 'theme', value: 'dkan'}) });
+    await act(async () => { jest.runAllTimers() });
+    expect(axios.get).toHaveBeenCalledWith(
+      `${rootUrl}/search/?theme=dkan`,
+    );
+    expect(result.current.selectedFacets).toEqual({theme: ['dkan']});
+    await act(async () => { result.current.setFulltext('blah') });
+    await act(async () => { jest.runAllTimers() });
+    expect(axios.get).toHaveBeenCalledWith(
+      `${rootUrl}/search/?fulltext=blah&theme=dkan`,
+    );
+    expect(result.current.selectedFacets).toEqual({theme: ['dkan']});
+    expect(result.current.fulltext).toEqual('blah');
+    await act(async () => { result.current.setPage(2) });
+    await act(async () => { jest.runAllTimers() });
+    expect(axios.get).toHaveBeenCalledWith(
+      `${rootUrl}/search/?fulltext=blah&page=3&theme=dkan`,
+    );
+    expect(result.current.selectedFacets).toEqual({theme: ['dkan']});
+    expect(result.current.fulltext).toEqual('blah');
+    expect(result.current.page).toEqual(2);
+
+    await act(async () => { result.current.resetFilters() });
+    await act(async () => { jest.runAllTimers() });
+    expect(axios.get).toHaveBeenCalledWith(
+      `${rootUrl}/search/?`,
+    );
+    expect(result.current.selectedFacets).toEqual({});
+    expect(result.current.fulltext).toEqual('');
+    expect(result.current.page).toEqual(0);
+  });
+
+  test('set hook with initial params', async() => {
+    const params = {
+      selectedFacets: {theme: ['dkan']},
+      sort: 'alpha',
+      page: 2,
+      pageSize: 25,
+      fulltext: 'react',
+      sortOrder: 'foobar'
+    }
+    axios.get.mockImplementation(() => Promise.resolve(data_results));
+    const { result } = renderHook(() => useSearchAPI(rootUrl, params));
+    await act(async () => { jest.runAllTimers() });
+    expect(axios.get).toHaveBeenCalledWith(
+      `${rootUrl}/search/?fulltext=react&page=3&page-size=25&sort=alpha&sort-order=foobar&theme=dkan`,
+    );
+    expect(result.current.pageSize).toEqual(25);
+    expect(result.current.selectedFacets).toEqual({theme: ['dkan']});
+    expect(result.current.page).toEqual(2);
+    expect(result.current.fulltext).toEqual('react');
+    expect(result.current.sort).toEqual('alpha');
+    expect(result.current.sortOrder).toEqual('foobar');
+  });
 });
