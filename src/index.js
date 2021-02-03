209a446eed78025b144a8ea64be8ddb06ba5356d
Brian Seek <brian.seek@civicactions.com>
2021-02-03 13:40:04 -0800

    Added useDrupalPage to index.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 export { default as useDatastore } from './hooks/useDatastore'
 export { default as useDatastoreSQL } from './hooks/useDatastoreSQL';
 export { default as useMetastoreDataset } from './hooks/useMetastoreDataset'
+export { useDrupalEntity } from './hooks/useDrupalPage'
 
 // REACT COMPONENTS and OUT OF BOX STUFF
 export { default as Resource } from './Resource'
