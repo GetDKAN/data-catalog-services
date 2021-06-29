f1ee067c1d718367fe7fc61195dc7b59266ea4a9
dgading <dgading@gmail.com>
2021-06-29 17:20:35 -0400

    Additional params are now an object

--- src/hooks/useMetastoreDataset/index.jsx
+++ src/hooks/useMetastoreDataset/index.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
+import qs from 'qs';
 import axios from 'axios';
 
 const useMetastoreDataset = (datasetId, rootAPIUrl, additionalParams) => {
   const [dataset, setDataset] = useState({});
   const [id, setId] = useState(datasetId)
   const [rootUrl, setRootUrl] = useState(rootAPIUrl)
+  const additionalParamsString = Object.keys(additionalParams).length ? `&${qs.stringify(additionalParams)}` : '';
   useEffect(() => {
     async function fetchData() {
-      return axios.get(`${rootUrl}/metastore/schemas/dataset/items/${id}?show-reference-ids${additionalParams}`)
+      return axios.get(`${rootUrl}/metastore/schemas/dataset/items/${id}?show-reference-ids${additionalParamsString}`)
         .then((res) => setDataset(res.data));
     }
     fetchData();
