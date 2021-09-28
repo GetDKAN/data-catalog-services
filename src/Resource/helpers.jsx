f0e07dfba286c0db9299b04d1e013a048327b340
dgading <dgading@gmail.com>
2021-09-28 10:20:49 -0400

    Remove legacy Resource component

--- src/Resource/helpers.jsx
+++ src/Resource/helpers.jsx
@@ -1,7 +1,3 @@
-import { createContext } from 'react';
-
-export const ResourceDispatch = createContext(null);
-
 // Build columns in correct structure for Datatable component.
 export function prepareColumns(columns) {
   return columns.map((column) => ({
