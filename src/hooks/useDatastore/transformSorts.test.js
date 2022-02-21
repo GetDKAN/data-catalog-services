2a0d25475d09b6be74d5ff86d9d630d2d2056a3d
Daniel <dgading@gmail.com>
2022-02-21 17:32:33 -0500

    Fix transform sort hook to work with new array of objects output

--- src/hooks/useDatastore/transformSorts.test.js
+++ src/hooks/useDatastore/transformSorts.test.js
@@ -1,12 +1,21 @@
-import { transformTableSortToQuerySort } from './transformSorts';
+import { transformTableSortToQuerySort } from "./transformSorts";
 
-describe('transformTableSortToQuerySort', () => {
-  test('transform an array from of sorts from React Table into DKAN query format', async () => {
-    const testArray1 = [{id: "my_label", desc: true}]
-    const testArray2 = [{id: "another_label", desc: false}]
+describe("transformTableSortToQuerySort", () => {
+  test("transform an array from of sorts from React Table into DKAN query format", async () => {
+    const testArray1 = [{ id: "my_label", desc: true }];
+    const testArray2 = [{ id: "another_label", desc: false }];
 
-    expect(transformTableSortToQuerySort(testArray1)).toEqual({asc: [], desc:['my_label']});
-    expect(transformTableSortToQuerySort(testArray2)).toEqual({asc: ['another_label'], desc:[]});
-    expect(transformTableSortToQuerySort(testArray1.concat(testArray2))).toEqual({asc: ['another_label'], desc:['my_label']});
+    expect(transformTableSortToQuerySort(testArray1)).toEqual([
+      { order: "desc", property: "my_label" },
+    ]);
+    expect(transformTableSortToQuerySort(testArray2)).toEqual([
+      { order: "asc", property: "another_label" },
+    ]);
+    expect(
+      transformTableSortToQuerySort(testArray1.concat(testArray2))
+    ).toEqual([
+      { order: "desc", property: "my_label" },
+      { order: "asc", property: "another_label" },
+    ]);
   });
 });
