fe5c2737d9a442ac1e7cc882f093c4c078b04a9a
Daniel <dgading@gmail.com>
2022-05-03 08:41:03 -0500

    Update useSearch useEffect to skip resetting to page 1 on initial render (#32)
    * Prevent useSearch from resetting to page 1 on initial render
    * Update axios and remove Eslint
    * Fix test for initial parameter search

--- src/hooks/useSearchAPI/index.jsx
+++ src/hooks/useSearchAPI/index.jsx
@@ -1,26 +1,50 @@
-import {useState, useEffect} from 'react';
-import { updateSelectedFacetObject, fetchDatasets } from './helpers';
+import { useState, useEffect } from "react";
+import { updateSelectedFacetObject, fetchDatasets } from "./helpers";
 
-const useSearchAPI = (rootUrl, initialSearchParams={}, additionalParams={}) => {
-  const defaultSort = '';
-  const defaultFulltext = '';
+const useSearchAPI = (
+  rootUrl,
+  initialSearchParams = {},
+  additionalParams = {}
+) => {
+  const defaultSort = "";
+  const defaultFulltext = "";
   const defaultSelectedFacets = {};
-  const defaultSortOrder = '';
+  const defaultSortOrder = "";
   const defaultPage = 1;
   const defaultPageSize = 10;
 
-  const sortOptions = ['modified', 'title'];
-  const sortOrderOptions = ['asc', 'desc'];
+  const sortOptions = ["modified", "title"];
+  const sortOrderOptions = ["asc", "desc"];
   const [items, setItems] = useState([]);
   const [facets, setFacets] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [totalItems, setTotalItems] = useState(null)
-  const [selectedFacets, setSelectedFacets] = useState(initialSearchParams.selectedFacets ? initialSearchParams.selectedFacets : defaultSelectedFacets)
-  const [fulltext, setFulltext] = useState(initialSearchParams.fulltext ? initialSearchParams.fulltext : defaultFulltext)
-  const [sort, setSort] = useState(initialSearchParams.sort ? initialSearchParams.sort : defaultSort);
-  const [sortOrder, setSortOrder] = useState(initialSearchParams.sortOrder ? initialSearchParams.sortOrder : defaultSortOrder)
-  const [page, setPage] = useState(initialSearchParams.page ? initialSearchParams.page : defaultPage);
-  const [pageSize, setPageSize] = useState(initialSearchParams.pageSize ? initialSearchParams.pageSize : defaultPageSize)
+  const [totalItems, setTotalItems] = useState(null);
+  const [selectedFacets, setSelectedFacets] = useState(
+    initialSearchParams.selectedFacets
+      ? initialSearchParams.selectedFacets
+      : defaultSelectedFacets
+  );
+  const [fulltext, setFulltext] = useState(
+    initialSearchParams.fulltext
+      ? initialSearchParams.fulltext
+      : defaultFulltext
+  );
+  const [sort, setSort] = useState(
+    initialSearchParams.sort ? initialSearchParams.sort : defaultSort
+  );
+  const [sortOrder, setSortOrder] = useState(
+    initialSearchParams.sortOrder
+      ? initialSearchParams.sortOrder
+      : defaultSortOrder
+  );
+  const [page, setPage] = useState(
+    initialSearchParams.page ? initialSearchParams.page : defaultPage
+  );
+  const [pageSize, setPageSize] = useState(
+    initialSearchParams.pageSize
+      ? initialSearchParams.pageSize
+      : defaultPageSize
+  );
 
   async function search() {
     const options = {
@@ -28,18 +52,18 @@ const useSearchAPI = (rootUrl, initialSearchParams={}, additionalParams={}) => {
       selectedFacets: selectedFacets,
       sort: sort,
       sortOrder: sortOrder,
-      page: (Number(page)),
-      pageSize: pageSize
-    }
+      page: Number(page),
+      pageSize: pageSize,
+    };
     const results = await fetchDatasets(rootUrl, options, additionalParams);
     const itemKeys = Object.keys(results.data.results);
     const itemsArray = itemKeys.map((key) => {
-      return results.data.results[key]
-    })
-    setFacets(results.data.facets)
-    setItems(itemsArray)
-    setTotalItems(results.data.total)
-    setLoading(false)
+      return results.data.results[key];
+    });
+    setFacets(results.data.facets);
+    setItems(itemsArray);
+    setTotalItems(results.data.total);
+    setLoading(false);
   }
 
   function resetFilters() {
@@ -54,16 +78,18 @@ const useSearchAPI = (rootUrl, initialSearchParams={}, additionalParams={}) => {
   }
 
   useEffect(() => {
-    setPage(1)
-  }, [fulltext, selectedFacets, pageSize])
+    if (totalItems !== null) {
+      setPage(1);
+    }
+  }, [fulltext, selectedFacets, pageSize]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(true)
-      search()
+      setLoading(true);
+      search();
     }, 1000);
     return () => clearTimeout(timer);
-  }, [fulltext, selectedFacets, sort, sortOrder, page, pageSize])
+  }, [fulltext, selectedFacets, sort, sortOrder, page, pageSize]);
 
   return {
     fulltext,
@@ -85,7 +111,7 @@ const useSearchAPI = (rootUrl, initialSearchParams={}, additionalParams={}) => {
     updateSelectedFacets,
     setFulltext,
     resetFilters,
-  }
-}
+  };
+};
 
 export default useSearchAPI;
